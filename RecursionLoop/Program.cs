221d75a2770f9d32143ccc524fea6675a8a7ac16
Anatoly Borisov <drfoxg@gmail.com>
2021-06-17 22:04:46 +0300

    Task Recursion 1

---
+++ RecursionLoop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    //
+    // Вывода чисел от a до b с использованием рекурсивного алгоритма
+    // Цикл for без ключевого слова for
+    //
+    static void Main(string[] args)
+    {
+        Loop(9, 19);
+
+        Console.WriteLine("\n\nPress any key to quit...");
+        Console.ReadKey();
+    }
+    static void Loop(int a, int b)
+    {
+        Console.Write($"Step {a} ");
+        if (a < b) Loop(a + 1, b);
+    }
+}
