519a1f05eb5483aa8eea6405212ee14894391b00
Anatoly Borisov <drfoxg@gmail.com>
2021-07-27 09:00:00 +0300

    fix C++ version support

--- findstr/findstr/findstr.cpp
+++ findstr/findstr/findstr.cpp
@@ -17,6 +17,11 @@ int main() {
 
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
+
+    cout << "Compiler version: " << _MSC_VER << endl;
+    
+    // /Zc:__cplusplus https://devblogs.microsoft.com/cppblog/msvc-now-correctly-reports-__cplusplus/
+    cout << "C++ version: " << __cplusplus << endl;
         
     cout << "Введити строку:" << endl;
 	cin >> input_str;
